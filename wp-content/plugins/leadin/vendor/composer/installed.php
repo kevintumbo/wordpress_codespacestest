23beed23865f7240191e95a5afc64400208534b3
Kevin Tumbo <kevin.tumbo39@gmail.com>
2024-08-21 18:12:18 +0300

    commit project

---
+++ wp-content/plugins/leadin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hubspot/leadin-wordpress-plugin',
+        'pretty_version' => '11.0.23',
+        'version' => '11.0.23.0',
+        'reference' => '620e18db187feccfea2ae1f3fe8134f23804e46c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hubspot/leadin-wordpress-plugin' => array(
+            'pretty_version' => '11.0.23',
+            'version' => '11.0.23.0',
+            'reference' => '620e18db187feccfea2ae1f3fe8134f23804e46c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
